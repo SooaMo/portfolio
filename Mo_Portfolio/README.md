20dc939b204b949d87dbfee3bee944f0e0ab5f46
SooaMo <112353298+SooaMo@users.noreply.github.com>
2022-08-28 15:28:50 -0700

    Add files via upload

---
+++ Mo_Portfolio/README.md
@@ -0,0 +1,8 @@
+# Starter Project
+
+This is a blank 'template' that you can copy to get started working on projects with. This template includes:
+
+- *.gitignore* for ignoring files git should not track 
+- *.gitattributes* for 'normalizing' line endings between operating systems and track large files with '[Large file storage](https://www.atlassian.com/git/tutorials/git-lfs)' (LFS)
+- Empty *_work*, *public_html*, *css*, *js*, and *img* folders
+- *README.md* this document explaining what the repository is for
---
+++ Mo_Portfolio/public_html/js/scroll.js
@@ -0,0 +1,30 @@
+//navBar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-10rem";
+  }
+  prevScrollpos = currentScrollPos;
+
+  scrollFunction()
+}
+
+//Get the button:
+mybutton = document.getElementById("topBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop >30 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
